a38ba7914a2375b6ad2ebf5b63b14550bdf53782
Roderick van Domburg <roderick@vandomburg.net>
2015-02-13 12:28:45 +0100

    Update to Laravel 5.0.4.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0ecfcafea38ce73f965d82b36be4ca39::getLoader();
+return ComposerAutoloaderInit3c7fdaddea34b13e294f45b0478d0558::getLoader();
