13cd1bcc48ed8d1efcb61b79b92cb319d1b44e31
Roderick van Domburg <roderick@vandomburg.net>
2015-02-15 14:57:41 +0100

    Remove unnecessary namespace imports.

--- app/Http/Controllers/MonitorController.php
+++ app/Http/Controllers/MonitorController.php
@@ -1,10 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
-
 use App\Contracts\Inverter;
 
 class MonitorController extends Controller {
