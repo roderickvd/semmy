20041bc3938d6ab7fdb6053429aef7a011e6633a
Roderick van Domburg <roderick@vandomburg.net>
2015-02-22 18:03:46 +0100

    Add PVOutput test.

---
+++ tests/Mocks/Responses/SaveResponse.php
@@ -0,0 +1,51 @@
+<?php namespace Responses;
+
+use App\Contracts\HTTP as HTTPContract;
+
+class SaveResponse implements HTTPContract {
+
+    /*
+	|--------------------------------------------------------------------------
+    | HTTP Response Saver
+    |--------------------------------------------------------------------------
+    |
+    | This mock saves Semmy's HTTP requests, so that they can be compared
+	| to a known string.
+    |
+    */
+
+	/**
+	 * The previously sent data.
+	 *
+	 * @var string
+	 */
+    public static $data;
+
+	/**
+	 * The previously sent headers.
+	 *
+	 * @var string
+	 */
+    public static $headers;
+
+	/**
+	 * Return the sample response.
+	 *
+	 * @param  string  $host
+	 * @param  string  $uri
+	 * @return string
+	 */
+    public static function get($host, $uri)
+    {
+		//
+    }
+
+	public static function post($host, $uri, $data, $headers)
+	{
+		self::$data    = http_build_query($data);
+		self::$headers = $headers;
+	}
+
+}
+
+?>
---
+++ tests/PVOutputTest.php
@@ -0,0 +1,35 @@
+<?php
+
+class PVOutputTest extends TestCase {
+
+	/**
+	 * Test that Semmy creates a correct status update.
+	 *
+	 * @return void
+	 */
+	public function testPVOutputUpdate()
+	{
+		$this->setResponse('', 'SaveResponse');
+		$http = $this->app->make('App\Contracts\HTTP');
+
+		$timestamp = time();
+		$time = str_replace(':', '%3A', date('H:i', $timestamp));
+		$date = date('Ymd', $timestamp);
+
+		$generation = $this->inverter->generation();
+		$ac_power   = $this->inverter->ac_power();
+		$dc_voltage = $this->inverter->dc_voltage();
+
+		$api_key = env('PVOUTPUT_API_KEY');
+		$sid     = env('PVOUTPUT_SID');
+
+		$this->app->make('App\Console\Commands\Loggers\PVOutputLogger')->update();
+
+		$this->assertContains("X-Pvoutput-Apikey: {$api_key}", $http::$headers);
+		$this->assertContains("X-Pvoutput-SystemId: {$sid}", $http::$headers);
+		$this->assertEquals("d={$date}&t={$time}&v1={$generation}&v2={$ac_power}&v6={$dc_voltage}", $http::$data);
+	}
+
+}
+
+?>
